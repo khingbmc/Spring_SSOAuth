c91277c03a7630022ee73810972940413881780a
KhingNoii <60070079@kmitl.ac.th>
2019-10-31 09:58:28 +0700

    Fix Pom

---
+++ DBManage/src/test/java/com/sso/mongo/DBManage/DbManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.mongo.DBManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
